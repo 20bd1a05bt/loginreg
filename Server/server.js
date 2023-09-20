0f617f4d9264e71be6b7bfeb65ae67e47b9704cf
20bd1a05bt <20bd1a05bt@gmail.com>
2023-09-20 17:35:26 +0530

    3rd commit

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const  app=express();
 const PORT=process.env.PORT||3000;
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{console.log(`Connect to DataBase`)})
+.catch((error)=>console.log(`${error.message}`));
+
 app.listen(PORT,()=>{
     console.log(`server running on ${PORT}`);
 })
